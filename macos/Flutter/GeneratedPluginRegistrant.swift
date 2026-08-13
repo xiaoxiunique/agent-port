3265d0c9c1415ff8f068a7ac53db2b91ad7c15a4
Atom <929746602@qq.com>
2026-08-13 11:18:13 +0800

    chore: register just_audio platform plugin
    Podfile.lock and the generated plugin registrants for iOS, macOS, Linux and
    Windows pick up the new just_audio plugin.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,9 @@
 import FlutterMacOS
 import Foundation
 
+import audio_session
 import file_selector_macos
+import just_audio
 import mobile_scanner
 import package_info_plus
 import shared_preferences_foundation
@@ -14,7 +16,9 @@ import url_launcher_macos
 import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
